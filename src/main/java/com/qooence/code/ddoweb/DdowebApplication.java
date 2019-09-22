e68e0cb748f6ddab8745d3f9afbfeafdde6ac982
Qooence <1044212759@qq.com>
2019-09-23 01:16:45 +0800

    first commit

---
+++ src/main/java/com/qooence/code/ddoweb/DdowebApplication.java
@@ -0,0 +1,13 @@
+package com.qooence.code.ddoweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdowebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DdowebApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/qooence/code/ddoweb/config/WebMvcConfig.java
@@ -0,0 +1,16 @@
+package com.qooence.code.ddoweb.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements  WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        // 前面是url路径，后面是视图路径，添加thymeleaf后自动配置prefix为/templates,suffix为.html
+        registry.addViewController("/login").setViewName("/login");
+        registry.addViewController("/home").setViewName("/home");
+    }
+}
